b2ed6bf398dc4d656b1a2b4ab93981addc7f7288
Rudhar <rcb35350@gmail.com>
2024-10-23 22:48:10 +0530

    ading email functinality

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+
+import nodemailer from 'nodemailer'
 import cookieParser from "cookie-parser"
 
 const app = express()
@@ -7,6 +9,7 @@ const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
+
 }))
 
 app.use(express.json({limit: "16kb"}))
@@ -15,7 +18,18 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
-//routes import
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure:true,
+    port:465,
+    auth: {
+      user: process.env.EMAIL_NAME,
+      pass: process.env.GMAIL_PASSWORD, 
+    },
+  });
+
+
+console.log(transporter)
 import userRouter from './routes/user.routes.js'
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
@@ -26,7 +40,7 @@ import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
 
-//routes declaration
+
 app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
@@ -39,4 +53,4 @@ app.use("/api/v1/dashboard", dashboardRouter)
 
 // http://localhost:8000/api/v1/users/register
 
-export { app }
+export { app,transporter }
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,11 +2,11 @@ import mongoose, {Schema} from "mongoose"
 
 const subscriptionSchema = new Schema({
     subscriber: {
-        type: Schema.Types.ObjectId, // one who is subscribing
+        type: Schema.Types.ObjectId, 
         ref: "User"
     },
     channel: {
-        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
+        type: Schema.Types.ObjectId, 
         ref: "User"
     }
 }, {timestamps: true})
